7105ccd6c9e13f8b06bdc6c281cb0aeaaeb751d5
Sabina Eraso <sabina.eraso05@gmail.com>
2024-07-22 17:49:46 +0000

    updates to book and book_recommender

--- demo1/src/book_recommender.ml
+++ demo1/src/book_recommender.ml
@@ -1,11 +1,29 @@
 open! Core
 
-let add_to_visited = ()
-let add_to_to_visit = ()
-let get_next_book = ()
+(* module State = struct type t = { visited : Hashtbl.M(Book).t ; to_visit :
+   Hashtbl.M(Book).t ; recommendations : Hashtbl.M(Book).t } end *)
 
-let take_user_input =
-  let _visited = [] in
-  let _to_visit = Hashtbl.create (module Book) in
+let visited = Hashtbl.create (module Int)
+let to_visit = Hashtbl.create (module Book)
+let recommendations = Hashtbl.create (module Book)
+
+let get_books_from_subject ~subject =
+  let books = [] in
+  (* file fetch subject to get list of books*)
+  List.iter books ~f:(fun (book : Book.t) ->
+    let isbn = Book.isbn book in
+    if not (Hashtbl.mem visited isbn)
+    then (
+      match Hashtbl.add to_visit ~key:book ~data:0 with
+      | `Ok -> Hashtbl.add_exn to_visit ~key:book ~data:1
+      | `Duplicate ->
+        Hashtbl.add_exn
+          to_visit
+          ~key:book
+          ~data:(Hashtbl.find_exn to_visit book + 1)))
+;;
+
+let get_next_book ~visited ~to_visit ~recommendations =
+  let new_book = Hashtbl.choose_randomly to_visit in
   ()
 ;;
