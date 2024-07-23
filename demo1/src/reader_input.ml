90fda05b0a500df227abf2d1460ebc8fac6bf7f0
Alex <awu245@gmail.com>
2024-07-23 16:29:02 +0000

    Alex Tues 12:20

--- demo1/src/reader_input.ml
+++ demo1/src/reader_input.ml
@@ -1,1 +1,28 @@
 open! Core
+
+let every (seconds : float) ~f ~stop =
+  let open Async in
+  let rec loop () =
+    if stop
+    then return ()
+    else
+      Clock.after (Time_float.Span.of_sec seconds)
+      >>= fun () ->
+      f ();
+      loop ()
+  in
+  don't_wait_for (loop ())
+;;
+
+let response_options =
+  Fzf.Pick_from.inputs
+    [ "Interested"
+    ; "Uninterested"
+    ; "Already Read; Didn't Like"
+    ; "Already Read; Did Like"
+    ; "Done"
+    ]
+;;
+
+let get_user_response = Fzf.pick_one response_options
+let start_game = print_string "Please input the name of your favorite book! "
