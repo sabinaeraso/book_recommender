990e20adbc524df9737bb0417b3605941aea2d60
Sabina Eraso <sabina.eraso05@gmail.com>
2024-07-23 13:48:00 +0000

    expect test

--- demo1/src/book_recommender.ml
+++ demo1/src/book_recommender.ml
@@ -10,7 +10,7 @@ module State = struct
 end
 
 (* user said yes to X book, so now we call this on all tis subjects : *)
-let _get_books_from_subject ~(state : State.t) ~subject =
+let get_books_from_subject ~(state : State.t) ~subject =
   let visited = state.visited in
   let to_visit = state.to_visit in
   let books_raw = Book_fetch.Fetcher.Subjects.fetch_sub subject in
@@ -22,12 +22,24 @@ let _get_books_from_subject ~(state : State.t) ~subject =
     then Book.Binary_heap.add to_visit book)
 ;;
 
-(* let%expect_test "Get books from subject" = let state = {State.visited =
-   []; to_visit = Book.Binary_heap.create 200; recommendations = []} in
-   get_books_from_subject ~state ~subject:"tooth_fairy";
-   Book.Binary_heap.iter to_visit
-
-   ;; *)
+let%expect_test "Get books from subject" =
+  let dummy =
+    Book.create
+      ~title:"Harry Potter"
+      ~key:"Key"
+      ~subjects:[]
+      ~isbn:(Some 1)
+      ~heuristic:1
+  in
+  let state =
+    { State.visited = []
+    ; to_visit = Book.Binary_heap.create ~dummy 200
+    ; recommendations = []
+    }
+  in
+  get_books_from_subject ~state ~subject:"tooth_fairy";
+  print_s [%message (state.to_visit : Book.Binary_heap.t)]
+;;
 
 let _get_next_book ~(state : State.t) =
   let new_book = Book.Binary_heap.pop_minimum state.to_visit in
