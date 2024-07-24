e0a29607e88f9004d360998b3804f4d2d9e4018d
Sabina Eraso <sabina.eraso05@gmail.com>
2024-07-24 20:12:03 +0000

    trying to not repeat searches to the same subject

--- demo1/src/book_recommender.ml
+++ demo1/src/book_recommender.ml
@@ -2,10 +2,11 @@ open! Core
 
 module State = struct
   type t =
-    { mutable visited : Book.Key.t list
+    { mutable visited_books : Book.Key.t list
     ; to_visit : Book.Binary_heap.t
     ; mutable recommendations : Book.t list
     ; mutable current_book : Book.t
+    ; mutable visited_subjects : string list
     }
   [@@deriving sexp_of]
 
@@ -14,10 +15,11 @@ module State = struct
       Book.create ~title:"Dummy" ~key:"Key" ~subjects:[] ~isbn:(Some 1)
     in
     let state =
-      { visited = []
+      { visited_books = []
       ; to_visit = Book.Binary_heap.create ~dummy 1
       ; recommendations = []
       ; current_book = dummy
+      ; visited_subjects = []
       }
     in
     state
@@ -26,16 +28,21 @@ end
 
 (* user said yes to X book, so now we call this on all tis subjects : *)
 let update_to_visit_from_subject ~(state : State.t) ~subject =
-  let visited = state.visited in
+  let visited_books = state.visited_books in
+  let visited_subjects = state.visited_subjects in
   let to_visit = state.to_visit in
-  let books_raw = Book_fetch.Fetcher.Subjects.fetch_sub subject in
-  let books = Page_parser.Subject_page.parse_books books_raw in
-  List.iter books ~f:(fun (book : Book.t) ->
-    let key = Book.key book in
-    book.heuristic <- book.heuristic + 1;
-    if not
-         (List.exists visited ~f:(fun k -> equal 0 (Book.Key.compare k key)))
-    then Book.Binary_heap.add to_visit book)
+  if not (List.exists visited_subjects ~f:(fun s -> String.equal s subject))
+  then (
+    state.visited_subjects <- List.append state.visited_subjects [ subject ];
+    let books_raw = Book_fetch.Fetcher.Subjects.fetch_sub subject in
+    let books = Page_parser.Subject_page.parse_books books_raw in
+    List.iter books ~f:(fun (book : Book.t) ->
+      let key = Book.key book in
+      book.heuristic <- book.heuristic + 1;
+      if not
+           (List.exists visited_books ~f:(fun k ->
+              equal 0 (Book.Key.compare k key)))
+      then Book.Binary_heap.add to_visit book))
 ;;
 
 let%expect_test "Get books from subject: Fantasy_fiction" =
@@ -46,7 +53,8 @@ let%expect_test "Get books from subject: Fantasy_fiction" =
 
 let get_next_book ~(state : State.t) =
   let new_book = Book.Binary_heap.pop_minimum state.to_visit in
-  state.visited <- List.append state.visited [ Book.key new_book ];
+  state.visited_books
+  <- List.append state.visited_books [ Book.key new_book ];
   new_book
 ;;
 
