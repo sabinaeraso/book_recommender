62fd68c8997b2d96a53d31e3665b744a66306700
Sabina Eraso <sabina.eraso05@gmail.com>
2024-07-31 14:44:59 +0000

    before changing everything in reader_input to fetch from google

--- demo1/src/handle.ml
+++ demo1/src/handle.ml
@@ -5,7 +5,6 @@ let handle_yes distance_from_origin ~(state : Book_recommender.State.t) =
   Book_recommender.update_visited ~state ~book;
   Book_recommender.update_recommendations ~state ~book;
   let subjects = book.subjects in
-  (* this needs to change *)
   List.iter subjects ~f:(fun subject ->
     let valid_subject =
       Or_error.try_with (fun () ->
@@ -19,11 +18,36 @@ let handle_yes distance_from_origin ~(state : Book_recommender.State.t) =
   Book_recommender.update_current_book ~state ~new_book:next_book
 ;;
 
-let handle_read_yes distance_from_origin ~(state : Book_recommender.State.t) =
+let handle_yes_google_api
+  distance_from_origin
+  ~(state : Book_recommender.State.t)
+  =
   let book = state.current_book in
   Book_recommender.update_visited ~state ~book;
-  let subjects = book.subjects in
-  (* this needs to change *)
+  Book_recommender.update_recommendations ~state ~book;
+  let book_page_raw = Google_api.Fetcher.fetch_book_by_id book.key in
+  let subjects = Google_api.Parser.get_categories_from_book book_page_raw in
+  List.iter subjects ~f:(fun subject ->
+    let valid_subject =
+      Or_error.try_with (fun () ->
+        Book_recommender.update_to_visit_from_subject
+          distance_from_origin
+          ~state
+          ~subject)
+    in
+    match valid_subject with Ok _ -> () | Error _ -> ());
+  let next_book = Book_recommender.get_next_book ~state in
+  Book_recommender.update_current_book ~state ~new_book:next_book
+;;
+
+let handle_read_yes_google_api
+  distance_from_origin
+  ~(state : Book_recommender.State.t)
+  =
+  let book = state.current_book in
+  Book_recommender.update_visited ~state ~book;
+  let book_page_raw = Google_api.Fetcher.fetch_book_by_id book.key in
+  let subjects = Google_api.Parser.get_categories_from_book book_page_raw in
   List.iter subjects ~f:(fun subject ->
     let valid_subject =
       Or_error.try_with (fun () ->
@@ -37,11 +61,22 @@ let handle_read_yes distance_from_origin ~(state : Book_recommender.State.t) =
   Book_recommender.update_current_book ~state ~new_book:next_book
 ;;
 
-(* sends the "distance from the origin" as 0.5 closer to the origin than in
-   acutality. this in turn means that the books found from the subjects of
-   this book will have a better (lower) heuristic, which is valid because we
-   know the reader actually enjoys this book, not only is interested in the
-   book.*)
+let handle_read_yes distance_from_origin ~(state : Book_recommender.State.t) =
+  let book = state.current_book in
+  Book_recommender.update_visited ~state ~book;
+  let subjects = book.subjects in
+  List.iter subjects ~f:(fun subject ->
+    let valid_subject =
+      Or_error.try_with (fun () ->
+        Book_recommender.update_to_visit_from_subject
+          (distance_from_origin -. 0.5)
+          ~state
+          ~subject)
+    in
+    match valid_subject with Ok _ -> () | Error _ -> ());
+  let next_book = Book_recommender.get_next_book ~state in
+  Book_recommender.update_current_book ~state ~new_book:next_book
+;;
 
 let handle_no ~(state : Book_recommender.State.t) =
   let book = state.current_book in
