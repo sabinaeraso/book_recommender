1a12b5d8576cd473c698ec82764bb69152d32609
Alex <awu245@gmail.com>
2024-08-05 19:24:51 +0000

    working file writing

--- demo1/src/cache.ml
+++ demo1/src/cache.ml
@@ -10,32 +10,38 @@ let is_in_cache t subject = Set.mem t.stored_subjects subject
 
 let write_to_cache t subject =
   t.stored_subjects <- Set.add t.stored_subjects subject;
-  (let raw_subject_page = Book_fetch.Fetcher.Subjects.fetch_sub subject in
-   Writer.save ("cache/" ^ subject ^ ".txt") ~contents:raw_subject_page)
-  |> ignore
+  let raw_subject_page = Book_fetch.Fetcher.Subjects.fetch_sub subject in
+  Writer.save ("cache/" ^ subject ^ ".txt") ~contents:raw_subject_page
 ;;
 
 (* need to ensure that the subject is already a created empty file before
    calling Writer.save*)
 
 let get_from_cache t subject =
-  if not (is_in_cache t subject) then write_to_cache t subject;
-  let%bind text = Reader.file_contents (subject ^ ".txt") in
-  return text
+  Deferred.bind
+    (if not (is_in_cache t subject)
+     then (
+       let%bind () = write_to_cache t subject in
+       return ())
+     else return ())
+    ~f:(fun () ->
+      let%bind text = Reader.file_contents ("cache/" ^ subject ^ ".txt") in
+      return text)
 ;;
 
 let write_file =
   let open Command.Let_syntax in
-  Command.basic
+  Async_command.async
     ~summary:"Given a subject writes it to cache"
     [%map_open
       let subject = flag "subject" (required string) ~doc:"Subject" in
       fun () ->
-        let fetched_file = Book_fetch.Fetcher.Subjects.fetch_sub subject in
         let sub =
           { stored_subjects = String.Set.of_list [ "hi" ]; _size = 0 }
         in
-        get_from_cache sub fetched_file |> ignore]
+        let%bind.Deferred text = get_from_cache sub subject in
+        print_endline text;
+        Deferred.return ()]
 ;;
 
 let command =
