a88d982ad72c6cbc6babda332c3255570849d573
Alex <awu245@gmail.com>
2024-08-07 18:50:16 +0000

    cache stores numbers

--- src/cache.ml
+++ src/cache.ml
@@ -46,7 +46,8 @@ let is_in_cache t subject =
   | None -> false
 ;;
 
-let below_limit t = t.size < 1000
+let limit = 40
+let below_limit t = t.size < limit
 
 let properly_formatted_subject (subject : string) =
   let banned_symbols =
@@ -90,7 +91,7 @@ let create_cache () : t Deferred.t =
   let empty_heap =
     Cache_item.Binary_heap.create
       ~dummy:(Cache_item.create ~title:"dummy" ~work_count:0)
-      1000
+      limit
   in
   let stored_subjects =
     List.fold text ~init:empty_heap ~f:(fun heap line ->
@@ -120,13 +121,31 @@ let add_subject_and_work_count_to_all_subject_titles
   return ()
 ;;
 
+let update_cache_file
+  (heap_of_cache : Cache_item.Binary_heap.t)
+  : unit Deferred.t
+  =
+  let%bind () = Writer.save_lines "cache/all_subject_titles.txt" [] in
+  Cache_item.Binary_heap.iter (fun item -> ) heap_of_cache
+  let%bind () =
+    Writer.save_lines
+      "cache/all_subject_titles.txt"
+      ([ cache_item.title ^ " " ^ Int.to_string cache_item.work_count ])
+  in
+  return ()
+;;
+
 let replace_min_subject t (cache_item : Cache_item.t) =
+  Core.print_endline "entered replacement subjects";
   let current_min = Cache_item.Binary_heap.minimum t.stored_subjects in
   let current_min_title = current_min.title in
   let current_min_count = current_min.work_count in
   if cache_item.work_count > current_min_count
   then (
     let _ = Cache_item.Binary_heap.pop_minimum t.stored_subjects in
+    Core.print_s [%sexp (current_min : Cache_item.t)];
+    Core.print_endline "replaced with";
+    Core.print_s [%sexp (cache_item : Cache_item.t)];
     Sys_unix.remove ("cache/" ^ current_min_title ^ ".txt");
     t.size <- t.size - 1)
   else ()
@@ -138,7 +157,7 @@ let write_to_cache t raw_subject_page formatted_subject =
       ~title:formatted_subject
       ~work_count:(Page_parser.Subject_page.get_work_count raw_subject_page)
   in
-  if equal t.size 1000 then replace_min_subject t cache_item;
+  if equal t.size limit then replace_min_subject t cache_item;
   if below_limit t
   then (
     match%bind
@@ -167,20 +186,21 @@ let write_to_cache t raw_subject_page formatted_subject =
 
 let get_from_cache t subject =
   let formatted_subject = format_filename subject in
-  let raw_subject_page =
-    Book_fetch.Fetcher.Subjects.fetch_sub ~limit:1000 subject
-  in
   if is_in_cache t formatted_subject
   then (
     let%bind text =
       Reader.file_contents ("cache/" ^ formatted_subject ^ ".txt")
     in
     return text)
-  else if properly_formatted_subject formatted_subject
-  then (
-    let%bind () = write_to_cache t raw_subject_page formatted_subject in
-    return raw_subject_page)
-  else return raw_subject_page
+  else (
+    let raw_subject_page =
+      Book_fetch.Fetcher.Subjects.fetch_sub ~limit:1000 subject
+    in
+    if properly_formatted_subject formatted_subject
+    then (
+      let%bind () = write_to_cache t raw_subject_page formatted_subject in
+      return raw_subject_page)
+    else return raw_subject_page)
 ;;
 
 let write_file =
